256ef4c6f57eb363f3dc8d9873f4884b8ff31488
Yann <yannco5@gmail.com>
2024-08-24 03:19:34 +0300

    splitting xlsx to csv using r script

---
+++ R files/01 csv from xlsx.R
@@ -0,0 +1,16 @@
+library(tidyverse)
+# load xlsx ---------------------------------------------------------------
+xl <- purrr::map(2:5,\(tab) readxl::read_xlsx('input files/matrixcorrect.xlsx', sheet = tab,skip = 1))
+xl <- xl |> purrr::set_names(c('all','pinks','greens','hetero'))
+
+# don't touch original xl, create a modified copy
+xlfmt <- xl
+
+# change item names
+for(i in 1:length(xlfmt)){
+  colnames(xlfmt[[i]]) <- paste(names(xlfmt)[i],colnames(xlfmt[[i]]),sep='_') |> str_remove_all('_...1')
+  xlfmt[[i]][,1] <- colnames(xlfmt[[i]])[-1]
+}
+
+# write csv ---------------------------------------------------------------
+walk2(xlfmt,names(xlfmt),\(df,df_name) write_csv(df,glue('input files/{df_name}.csv')))
