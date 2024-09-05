97cb12f8a4e6ce585a785882cfb8ae6befe653cc
Yann <yannco5@gmail.com>
2024-09-05 22:29:34 +0300

    editing applu

--- Py files/03 apply.py
+++ Py files/03 apply.py
@@ -1,15 +1,11 @@
 import sys
 import os
 import importlib.util
-# Get the directory of the current script
+
 current_dir = os.path.dirname(os.path.abspath(__name__))
-
-
-# Construct the full path to the file you want to import
+# full path
 file_path = os.path.join(current_dir, "01 network init.py")
-
-# Use importlib to import the module
+# load file
 spec = importlib.util.spec_from_file_location("network_init", file_path)
 network_init = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(network_init)
-print(net_name)
